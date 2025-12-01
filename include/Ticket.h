1c638a49650aec492b2ae7151042fc2b70b4af0d
Abhinav2370 <abhinavmenon217@gmail.com>
2025-12-01 17:20:29 +0530

    header file
    Updated Ticket.h to include railway definitions and function declarations.

--- include/Ticket.h
+++ include/Ticket.h
@@ -1,29 +1,24 @@
-#ifndef TICKET_H
-#define TICKET_H
+#ifndef RAILWAY_H
+#define RAILWAY_H
 
-#define MAX 5
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 5   // max 5 tickets
+#define TRAIN_COUNT 3
 
 struct Ticket {
     char name[30];
     char train[30];
     int age;
     int booked;   // 1 = booked, 0 = empty
-    float price;  // Ticket price
 };
 
-// Famous trains with predefined prices (AC/Non-AC per person)
-struct TrainPrice {
-    char name[30];
-    float ac_price;
-    float non_ac_price;
-};
-
-extern struct TrainPrice trains[];
-
-// Function prototypes
-void bookTicket(struct Ticket t[]);
-void viewTickets(struct Ticket t[]);
-void cancelTicket(struct Ticket t[]);
+// Function declarations
+void init(struct Ticket t[]);
+void display(struct Ticket t[]);
+void book(struct Ticket t[]);
+void cancel(struct Ticket t[]);
 void showTrains();
 
 #endif
