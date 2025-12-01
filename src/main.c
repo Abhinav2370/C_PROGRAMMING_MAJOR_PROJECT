1770c54f5d92537992f3f1ef12247259ecadec56
Abhinav2370 <abhinavmenon217@gmail.com>
2025-12-01 16:09:51 +0530

    C_file

--- src/main.c
+++ src/main.c
@@ -1,94 +1,41 @@
-
 #include <stdio.h>
-#include <string.h>
-
-#define MAX 5  
-
-struct Ticket {
-    char name[30];
-    char train[30];
-    int age;
-    int booked;   // 1 = booked, 0 = empty
-};
+#include "ticket.h"
 
 int main() {
     struct Ticket t[MAX];
-    int choice;
-    int count = 0;
-
     
     for (int i = 0; i < MAX; i++) {
         t[i].booked = 0;
     }
 
+    int choice;
+    
     while (1) {
         printf("\n===== Railway Ticket Management System =====\n");
-        printf("1. Book Ticket\n");
-        printf("2. View All Tickets\n");
-        printf("3. Cancel Ticket\n");
-        printf("4. Exit\n");
+        printf("1. View Available Trains\n");
+        printf("2. Book Ticket\n");
+        printf("3. View All Tickets\n");
+        printf("4. Cancel Ticket\n");
+        printf("5. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-       
         if (choice == 1) {
-            if (count >= MAX) {
-                printf("\nAll tickets are booked!\n");
-            } else {
-                printf("\nEnter Name: ");
-                scanf("%s", t[count].name);
-
-                printf("Enter Train Name: ");
-                scanf("%s", t[count].train);
-
-                printf("Enter Age: ");
-                scanf("%d", &t[count].age);
-
-                t[count].booked = 1;
-                printf("\nTicket Booked! Ticket ID = %d\n", count + 1);
-                count++;
-            }
+            showTrains();
         }
-
-        
         else if (choice == 2) {
-            printf(" All Booked Tickets\n");
-
-            int found = 0;
-            for (int i = 0; i < MAX; i++) {
-                if (t[i].booked == 1) {
-                    printf("\nTicket ID: %d\n", i + 1);
-                    printf("Name: %s\n", t[i].name);
-                    printf("Train: %s\n", t[i].train);
-                    printf("Age: %d\n", t[i].age);
-                    found = 1;
-                }
-            }
-
-            if (!found) {
-                printf("No tickets booked yet.\n");
-            }
+            bookTicket(t);
         }
-
-        else if (choice ==3) {
-            int id;
-            printf("\nEnter Ticket ID to cancel: ");
-            scanf("%d", &id);
-
-            if (id < 1 || id > MAX || t[id - 1].booked == 0) {
-                printf("\nInvalid Ticket ID!\n");
-            } else {
-                t[id - 1].booked = 0;
-                printf("Ticket %d cancelled.\n", id);
-            }
+        else if (choice == 3) {
+            viewTickets(t);
         }
-
-        // EXIT
         else if (choice == 4) {
+            cancelTicket(t);
+        }
+        else if (choice == 5) {
             printf("\nExiting... Thank you!\n");
             break;
         }
-
         else {
             printf("\nInvalid choice! Try again.\n");
         }
